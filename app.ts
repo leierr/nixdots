098d036e50d2b984b961c0ef36efe8fefa83dc90
Lars Smith Eier <hBm5BEqULhwPKUY@protonmail.com>
2025-07-09 14:38:03 +0200

    new way of doing on bar per monitor

--- app.ts
+++ app.ts
@@ -6,12 +6,6 @@ import Astal from "gi://Astal?version=4.0"
 import { execAsync } from "ags/process"
 import { monitorFile } from "ags/file"
 
-function createMonitorWindows(monitor: Gdk.Monitor) {
-  return [
-    topbar(monitor),
-  ];
-}
-
 (async () => {
   const pathsToMonitor = [`${SRC}/style` ]
   const mainScss = `${SRC}/style/main.scss` // SCSS input file to compile
@@ -40,27 +34,38 @@ function createMonitorWindows(monitor: Gdk.Monitor) {
   return transpileAndApply()
 })()
 
+function createMonitorWindows(monitor: Gdk.Monitor) {
+  return [
+    topbar(monitor),
+  ];
+}
+
 function main() {
-  const windowsByMonitor = new Map<Gdk.Monitor, Gtk.Window[]>();
+  const windowsPerMonitor = new Map()
 
   const reconcileMonitors = () => {
-    const activeMonitors = new Set(app.get_monitors());
+    const currentMonitors = app.get_monitors()
+    const activeConnectors = new Set()
 
-    activeMonitors.forEach(monitor => {
-      if (!windowsByMonitor.has(monitor))
-        windowsByMonitor.set(monitor, createMonitorWindows(monitor));
+    currentMonitors.forEach(monitor => {
+      const key = monitor.get_connector()
+      activeConnectors.add(key)
+
+      if (!windowsPerMonitor.has(key)) {
+        windowsPerMonitor.set(key, createMonitorWindows(monitor))
+      }
     });
 
-    for (const [monitor, windows] of windowsByMonitor) {
-      if (!activeMonitors.has(monitor)) {
-        windows.forEach(w => w.destroy());
-        windowsByMonitor.delete(monitor);
+    for (const [key, wins] of windowsPerMonitor) {
+      if (!activeConnectors.has(key)) {
+        wins.forEach((w: Gtk.Window) => w.destroy())
+        windowsPerMonitor.delete(key)
       }
     }
   };
 
-  reconcileMonitors();
-  app.connect('notify::monitors', reconcileMonitors);
+  reconcileMonitors()
+  app.connect('notify::monitors', reconcileMonitors)
 }
 
-app.start({ css: style, main });
+app.start({ css: style, main })
